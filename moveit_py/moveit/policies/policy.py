c264902a96b72e45fb3f36076d5191dc8330644f
peterdavidfagan <peterdavidfagan@gmail.com>
2023-10-27 11:16:00 +0100

    init policy class

--- moveit_py/moveit/policies/policy.py
+++ moveit_py/moveit/policies/policy.py
@@ -38,60 +38,88 @@ from abc import ABC, abstractmethod
 import rclpy
 from rclpy.node import Node
 
+from geometry_msgs.msg import PoseStamped, Twist
+from sensor_msgs.msg import Image
+
+from std_srvs.srv import SetBool
+
 import message_filters
 
 class Policy(ABC, Node):
     """An abstract base class for deploying learnt policies."""
     
-    def __init__(self, cfg):
+    def __init__(self, params, node_name="policy_node"):
         """Initialise the policy."""
-        super().__init__()
+        super().__init__(node_name)
+        self.logger = self.get_logger()        
+        self.param_listener = params.ParamListener(self)
+        self.params = self.param_listener.get_params()
         
         # status of policy rollout
         self.active = False
-
-        # register sensor topics
-        self.register_sensors_topics(cfg.sensors)
         
-        # register actuator topics
-        self.register_actuator_topics(cfg.actuators)
+        # register sensor topics
+        self.register_sensors()
+        
+        # register controller topics
+        self.register_controller()
         
         # TODO: create service for pausing and resuming policy execution
+        self.activate_policy = self.create_service( 
+            SetBool,
+            "activate_policy",
+            self.activate_policy,
+            )
 
-    def register_sensors_topics(self, sensor_cfg):
+    def get_sensor_msg_type(self, msg_type):
+        if msg_type == "sensor_msgs/Image":
+            return Image
+        else:
+            raise ValueError("Sensor type {} not supported.".format(msg_type))
+
+    def get_controller_msg_type(self, msg_type):
+        if msg_type == "geometry_msgs/PoseStamped":
+            return PoseStamped
+        elif msg_type == "geometry_msgs/Twist":
+            return Twist
+        else:
+            raise ValueError("Controller type {} not supported.".format(msg_type))
+
+    def register_sensors(self):
         """Register the topics to listen to for sensor data."""
-        topic_subs = []
-        for sensor in sensor_cfg.sensors:
-            topic_subs.append(message_filters.Subscriber(
+        self.sensor_subs = []
+        for sensor_idx in range(self.params.num_sensors):
+            # TODO: refactor this section  !!!
+            sensor_params = self.get_parameters_by_prefix("sensor{}".format(sensor_idx+1))
+            self.sensor_subs.append(message_filters.Subscriber(
                     self, 
-                    sensor.type, 
-                    sensor.topic,
-                    sensor.qos,
+                    self.get_sensor_msg_type(sensor_params["type"].value), 
+                    str(sensor_params["topic"].value),
+                    #int(sensor_params["qos"].value),
                     ))
 
         # create a synchronizer for the sensor topics
         self.sensor_sync = message_filters.ApproximateTimeSynchronizer(
-            topic_subs,
-            sensor_cfg.queue_size,
-            sensor_cfg.slop,
+            self.sensor_subs,
+            self.params.sensor_queue,
+            self.params.sensor_slop,
             )
+        
+        # register model forward pass as callback
         self.sensor_sync.registerCallback(self.forward)
 
-    def register_actuator_topics(self, action_cfg):
+    def register_controller(self):
         """Register the topic to publish actions to."""
-        self.action_pub = self.create_publisher(
-            action_cfg.type,
-            action_cfg.topic,
-            action_cfg.qos,
+        self.controller_pub = self.create_publisher(
+            self.get_controller_msg_type(self.params.controller1.type),
+            self.params.controller1.topic,
+            self.params.controller1.qos,
             )
 
-    def pause_execution(self):
-        """Pause the execution of the policy."""
-        self.active = False
-
-    def resume_execution(self):
-        """Resume the execution of the policy."""
-        self.active = True
+    def activate_policy(self, request, response):
+        """Activate the policy."""
+        self.active = request.data
+        return response
 
     @abstractmethod
     def forward():
